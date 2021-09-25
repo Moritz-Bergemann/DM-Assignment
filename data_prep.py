7a6dd7651989a343e39c2f11c8ec6cb0a71c4b45
Moritz Bergemann <49220792+Moritz-Bergemann@users.noreply.github.com>
2021-09-25 14:39:12 +0800

    feat: More data prep

--- data_prep.py
+++ data_prep.py
@@ -11,7 +11,19 @@ def main():
     # Remove the 'ID' index (since it is useless and interferes with everything else)
     df = df.drop(['ID'], axis=1)
 
+    ## Type Conversions
+    # Convert attributes with less than 30 unique values to categorical
+    lt_30_unique_cols = [col for col in df.columns if df[col].nunique() <= 30]
+    df[lt_30_unique_cols] = df[lt_30_unique_cols].astype('category')
+
+    # Convert numeric attributes to categorical through binning
+    numeric_cols = df.select_dtypes(include=['int64', 'float64']).columns
+    for col in numeric_cols:
+        pd.cut(df[col], bins=10)
+        df[col] = pd.cut(df[col], bins=10)
+
     ## Removing duplicate rows
+    print("Removing duplicate rows...")
     df = df.drop_duplicates()
 
     ## Removing duplicate columns
@@ -20,12 +32,31 @@ def main():
     df = df.drop(list(df.columns[duplicate_cols]), axis=1)
 
     ## Removing Useless Columns
-    print(f"Removing known 'duplicate' rows (100% correlation) ")
+    # Remove high-correlation columns
+    print(f"Removing second of known 'duplicate' columns (100% correlation): [C4, C25]")
+    df = df.drop(['C25'], axis=1)
+
+    # Removing columns with >80% missing values    
+    cols_missing_values = df.isnull().mean()
+    cols_missing_values_80 = cols_missing_values[cols_missing_values > 0.8]
+    print(f"Removing columns with more than 80% missing values: {list(cols_missing_values_80.index)}")
+    df = df.drop(cols_missing_values_80.index, axis=1)
+
+    # Removing columns with all the same values
+    cols_nunique_1 = df.nunique()[df.nunique() == 1]
+    df = df.drop(cols_nunique_1.index, axis=1)
+    print(f"Removing columns with only 1 unique value: {list(cols_nunique_1.index)}")
+
+    # Fill in null values with column mode
+    print(f"Filling missing values with column node")
+    for col in df.columns:
+        df[col] = df[col].fillna(value=df[col].mode())
+
+    print("Done! Saving...")
 
     ## Final saving
     df_in.to_csv('df_in.csv', index=False)
     df.to_csv('df_out.csv', index=False)
-
 
 
 if __name__ == '__main__':
