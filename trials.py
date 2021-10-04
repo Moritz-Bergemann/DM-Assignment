38b7b08e0f98d278c03d48e983dc43cb86e7f258
Moritz Bergemann <49220792+Moritz-Bergemann@users.noreply.github.com>
2021-10-04 22:50:25 +0800

    meta: Before attempting move to numpy

--- trials.py
+++ trials.py
@@ -11,12 +11,14 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from sklearn.model_selection import train_test_split, cross_val_score, cross_validate, StratifiedKFold, KFold, cross_val_predict, GridSearchCV
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.compose import ColumnTransformer
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from imblearn.over_sampling import SMOTE, SMOTEN
+from imblearn.under_sampling import RandomUnderSampler, EditedNearestNeighbours, TomekLinks, ClusterCentroids
 from imblearn.pipeline import Pipeline as ImbPipeline
 
 
@@ -25,18 +27,20 @@ import data_prep
 RANDOM_STATE = 123
 # TODO don't be a perfectionist!
 
+np.random.seed(0)
+
 # Non Sklearn-adjustable hyperparams go in here!!!
-def trial(model:str, param_grid:dict, drop_attributes:list, sampling:str, bins:int): # TODO pick h-params to adjust
+def trial(model:str, param_grid:dict, drop_attributes:list, sampling:str, bins:int, verbosity=2, san_check=True, save_folder='trials'): # TODO pick h-params to adjust
     df_train, _ = data_prep.get_prepped_dataset(bins=bins)
 
     ## Input validation
-    assert sampling in ['none', 'over', 'under', 'both']
+    assert sampling in ['none', 'smote', 'random-under', 'smote-random-under', 'smote-enn', 'smote-tomek']
     assert model in ['knn', 'dt', 'nbayes', 'svm']
     if drop_attributes != None:
         assert set(drop_attributes) <= set(list(df_train.columns)) # Each col to drop must be in cols
 
-    if not os.path.isdir("./trials"):
-        os.path.mkdir("./trials")
+    if not os.path.isdir(f"./{save_folder}"):
+        os.path.mkdir(f"./{save_folder}")
 
     ## Prepare Dataset
     # Split into labels/not labels
@@ -59,22 +63,31 @@ def trial(model:str, param_grid:dict, drop_attributes:list, sampling:str, bins:i
     pipe_parts = []
 
     # Sampling strategy
-    if bins != None: # If everything binned
-        if sampling == 'over':
-            pipe_parts.append(('smoten', SMOTEN(random_state=RANDOM_STATE, sampling_strategy='not majority')))
-        elif sampling == 'under':
+    if sampling != 'none':
+        if bins != None: # If everything binned
+            if sampling == 'smote':
+                pipe_parts.append(('smoten', SMOTEN(sampling_strategy=1)))
+            elif sampling == 'random-under':
+                pipe_parts.append(('undersample', RandomUnderSampler(sampling_strategy=0.5)))
+            elif sampling == 'smote-random-under':
+                pipe_parts.append(('smoten', SMOTEN(sampling_strategy=0.45)))
+                pipe_parts.append(('undersample', RandomUnderSampler(sampling_strategy=0.6)))
+            # elif sampling == 'smote-enn':
+            #     pipe_parts.append(('smoten', SMOTEN(sampling_strategy=0.45)))
+            #     pipe_parts.append(('enn', EditedNearestNeighbours(sampling_strategy=0.6)))
+            # elif sampling == 'smote-tomek':
+            #     pipe_parts.append(('smoten', SMOTEN(sampling_strategy=0.45)))
+            #     pipe_parts.append(('enn', TomekLinks(sampling_strategy=0.6)))
+
+        else: # Add Smote-Mixed if things not binned
             raise NotImplementedError() # TODO
-        elif sampling == 'both':
-            raise NotImplementedError() # TODO
-    else: # Add Smote-Mixed if things not binned
-        raise NotImplementedError() # TODO
     
     # One-hot encoding
     if bins != None: # Use onehot encoder if everything binned
         categories = [list(df_X[col].cat.categories) for col in df_X.columns]
         pipe_parts.append(('onehot', OneHotEncoder(categories=categories)))
     else: # Use mixed encoder if not everything binned
-        raise NotImplementedError() # TODO
+            raise NotImplementedError() # TODO
     
     # Model itself
     if model =='knn':
@@ -100,32 +113,126 @@ def trial(model:str, param_grid:dict, drop_attributes:list, sampling:str, bins:i
     # nbayes - TODO
     # svm - TODO
 
-    grid_search = GridSearchCV(pipe, param_grid, cv=skfold, verbose=2)
+    grid_search = GridSearchCV(pipe, param_grid, cv=skfold, verbose=verbosity)
 
     print("[i] Starting train...")
     start_time = dt.datetime.now()
     grid_search.fit(X, y)
     print(f"[i] Training complete! Elapsed time - {(dt.datetime.now() - start_time).total_seconds()}s")
 
+    ## Save results
     # Make save string
     if drop_attributes == None or len(drop_attributes) == 0:
         drop_save_string = "none"
     else:
         drop_save_string = f"[{','.join(drop_attributes)}]"
-    save_path = f"./trials/m={model}-s={sampling}-b={bins},d={drop_save_string}.pickle"
+    save_path = f"./{save_folder}/m={model}-s={sampling}-b={bins},d={drop_save_string}.pickle"
 
     print(f"[i] Trial: Saving results to {save_path}")
     with open(save_path, 'wb') as f:
         pickle.dump(grid_search, f)
+    
+    if san_check:
+        print(f"Sanity Check: {sanity_check(grid_search, drop_attributes=drop_attributes, bins=bins)}")
+
+    return grid_search
+
+def sanity_check(result:GridSearchCV, drop_attributes, bins):
+    ## Get the dataset
+    df_train, _ = data_prep.get_prepped_dataset(bins=bins)
+
+    ## Prepare Dataset
+    # Split into labels/not labels
+    df_X = df_train.drop('Class', axis=1)
+    df_y = df_train['Class']
+
+    # Drop attributes to drop
+    if drop_attributes != None and len(drop_attributes) != 0:
+        df_X = df_X.drop(drop_attributes, axis=1)
+
+    X = df_X.to_numpy()
+    y = df_y.to_numpy()
+
+    # Encode labels
+    y = LabelEncoder().fit_transform(y)
+
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=123)
+
+    estimator = result.best_estimator_
+    estimator.fit(X_train, y_train)
+
+    perf = accuracy_score(estimator.predict(X_val), y_val)
+
+    return perf
+
 
 def main():
     param_grid = {
-        'model_dt__criterion': ['gini', 'entropy'],
+        'model_knn__n_neighbors': [3, 5, 7, 9]
+    }
+
+    print("No Sampling Strategy")
+    result = trial(model='dt', param_grid=param_grid, drop_attributes=None, sampling='none', bins=None, verbosity=0)
+    print(result.best_score_)
+
+def main2():
+    # OLD STUFF
+    param_grid = {
+        'model_dt__criterion': ['entropy'],
         'model_dt__min_samples_split': [2, 10, 40],
         'model_dt__min_samples_leaf': [1, 10, 20]
     }
 
-    trial(model='dt', param_grid=param_grid, drop_attributes=None, sampling='over', bins=10)
+    print("No Sampling Strategy")
+    result = trial(model='dt', param_grid=param_grid, drop_attributes=None, sampling='none', bins=10, verbosity=0)
+    print(result.best_score_)
+
+    print("Undersampling (Random)")
+    result = trial(model='dt', param_grid=param_grid, drop_attributes=None, sampling='random-under', bins=10, verbosity=0)
+    print(result.best_score_)
+
+    print("Oversampling")
+    result = trial(model='dt', param_grid=param_grid, drop_attributes=None, sampling='smote', bins=10, verbosity=0)
+    print(result.best_score_)
+
+    print("SMOTE & Random Undersampling")
+    result = trial(model='dt', param_grid=param_grid, drop_attributes=None, sampling='smote-random-under', bins=10, verbosity=0)
+    print(result.best_score_)
+
+    param_grid = {
+        'model_knn__n_neighbors': [3, 5, 7, 9]
+    }
+
+    print("No Sampling Strategy")
+    result = trial(model='knn', param_grid=param_grid, drop_attributes=None, sampling='none', bins=10, verbosity=0)
+    print(f"Best Score: {result.best_score_}")
+    print(f"Sanity check: {sanity_check(result, drop_attributes=None, bins=10)}")
+
+    print("Undersampling (Random)")
+    result = trial(model='knn', param_grid=param_grid, drop_attributes=None, sampling='random-under', bins=10, verbosity=0)
+    print(f"Best Score: {result.best_score_}")
+    print(f"Sanity check: {sanity_check(result, drop_attributes=None, bins=10)}")
+
+    print("Oversampling")
+    result = trial(model='knn', param_grid=param_grid, drop_attributes=None, sampling='smote', bins=10, verbosity=0)
+    print(f"Best Score: {result.best_score_}")
+    print(f"Sanity check: {sanity_check(result, drop_attributes=None, bins=10)}")
+
+    print("SMOTE & Random Undersampling")
+    result = trial(model='knn', param_grid=param_grid, drop_attributes=None, sampling='smote-random-under', bins=10, verbosity=0)
+    print(f"Best Score: {result.best_score_}")
+    print(f"Sanity check: {sanity_check(result, drop_attributes=None, bins=10)}")
+
+    print("SMOTE & ENN Undersampling")
+    result = trial(model='knn', param_grid=param_grid, drop_attributes=None, sampling='smote-enn', bins=10, verbosity=0)
+    print(f"Best Score: {result.best_score_}")
+    print(f"Sanity check: {sanity_check(result, drop_attributes=None, bins=10)}")
+
+    print("SMOTE & Tomek Undersampling")
+    result = trial(model='knn', param_grid=param_grid, drop_attributes=None, sampling='smote-tomek', bins=10, verbosity=0)
+    print(f"Best Score: {result.best_score_}")
+    print(f"Sanity check: {sanity_check(result, drop_attributes=None, bins=10)}")
+
 
 if __name__ == '__main__':
     main()
