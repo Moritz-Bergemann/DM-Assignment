2b502fee0bf1743383d859bd3bdd856a2e9be685
Moritz Bergemann <49220792+Moritz-Bergemann@users.noreply.github.com>
2021-10-04 16:13:20 +0800

    fix: timing & path issues

--- trials.py
+++ trials.py
@@ -4,6 +4,9 @@
 ## ATTEMPT AT FULL FUNCTIONS ##
 ###############################
 
+import pickle 
+import datetime as dt
+import os
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -11,6 +14,7 @@ from sklearn.model_selection import train_test_split, cross_val_score, cross_val
 from sklearn.metrics import confusion_matrix
 from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 from imblearn.over_sampling import SMOTE, SMOTEN
 from imblearn.pipeline import Pipeline as ImbPipeline
@@ -22,25 +26,31 @@ RANDOM_STATE = 123
 # TODO don't be a perfectionist!
 
 # Non Sklearn-adjustable hyperparams go in here!!!
-def trial(model:str, drop_attributes:list, sampling:str, bins:int, cat_binning_threshold:int): # TODO pick h-params to adjust
-    df_train, _ = data_prep.get_prepped_dataset(bins=bins, cat_binning_threshold=cat_binning_threshold)
+def trial(model:str, drop_attributes:list, sampling:str, bins:int): # TODO pick h-params to adjust
+    df_train, _ = data_prep.get_prepped_dataset(bins=bins)
 
-    # Input validation
+    ## Input validation
     assert sampling in ['none', 'over', 'under', 'both']
     assert model in ['knn', 'dt', 'nbayes', 'svm']
-    assert set(drop_attributes) <= set(list(df_train.columns)) # Each col to drop must be in cols
+    if drop_attributes != None:
+        assert set(drop_attributes) <= set(list(df_train.columns)) # Each col to drop must be in cols
+
+    if not os.path.isdir("./trials"):
+        os.path.mkdir("./trials")
 
     ## Prepare Dataset
-
     # Split into labels/not labels
     df_X = df_train.drop('Class', axis=1)
     df_y = df_train['Class']
 
     # Drop attributes to drop
-    df_X = df_X.drop(drop_attributes, axis=1)
+    if drop_attributes != None and len(drop_attributes) != 0:
+        df_X = df_X.drop(drop_attributes, axis=1)
 
     X = df_X.to_numpy()
     y = df_y.to_numpy()
+
+    print("[i] Loaded dataset")
 
     # Encode labels
     y = LabelEncoder().fit_transform(y)
@@ -61,15 +71,16 @@ def trial(model:str, drop_attributes:list, sampling:str, bins:int, cat_binning_t
     
     # One-hot encoding
     if bins != None: # Use onehot encoder if everything binned
-        pipe_parts.append('onehot', OneHotEncoder())
+        categories = [list(df_X[col].cat.categories) for col in df_X.columns]
+        pipe_parts.append(('onehot', OneHotEncoder(categories=categories)))
     else: # Use mixed encoder if not everything binned
         raise NotImplementedError() # TODO
     
     # Model itself
     if model =='knn':
-        pipe_parts.append('model_knn', KNeighborsClassifier())
+        pipe_parts.append(('model_knn', KNeighborsClassifier()))
     if model =='dt':
-        pipe_parts.append('model_dt', DecisionTreeClassifier())
+        pipe_parts.append(('model_dt', DecisionTreeClassifier()))
     if model =='nbayes':
         raise NotImplementedError
         pipe_parts.append('model_nbayes', ) # TODO
@@ -84,13 +95,13 @@ def trial(model:str, drop_attributes:list, sampling:str, bins:int, cat_binning_t
     if model =='knn':
         param_grid = {
             # All odd values for k-neighbors between 3 & 22
-            'knn__k_neighbors': [ii for ii in range(3, 22, 2)]
+            'model_knn__k_neighbors': [ii for ii in range(3, 22, 2)]
         }
     if model =='dt':
         param_grid = {
-            'dt__criterion': ['gini', 'entropy'],
-            'dt__min_samples_split': [ii for ii in range(2, 41)],
-            'dt__min_samples_leaf': [ii for ii in range(1, 21)]
+            'model_dt__criterion': ['gini', 'entropy'],
+            'model_dt__min_samples_split': [ii for ii in range(2, 41)],
+            'model_dt__min_samples_leaf': [ii for ii in range(1, 21)]
         }
     if model =='nbayes':
         param_grid = {
@@ -104,10 +115,26 @@ def trial(model:str, drop_attributes:list, sampling:str, bins:int, cat_binning_t
     # Define K-fold sampling
     skfold = StratifiedKFold(n_splits=10)
     
-    grid_search = GridSearchCV(pipe, param_grid, cv=skfold, verbose=1)
+    grid_search = GridSearchCV(pipe, param_grid, cv=skfold, verbose=2)
 
+    print("[i] Starting train...")
+    start_time = dt.datetime.now()
     grid_search.fit(X, y)
+    print(f"[i] Training complete! Elapsed time - {(dt.datetime.now() - start_time).total_seconds}s")
 
-    save_path = f"./trials/m={model}-s={sampling}-b={bins},d=[{','.join(drop_attributes)}]"
+    # Make save string
+    if drop_attributes == None or len(drop_attributes) == 0:
+        drop_save_string = "none"
+    else:
+        drop_save_string = f"[{','.join(drop_attributes)}]"
+    save_path = f"./trials/m={model}-s={sampling}-b={bins},d={drop_save_string}.pickle"
 
-    print(f"Trial: Saving results to {save_path}")
+    print(f"[i] Trial: Saving results to {save_path}")
+    with open(save_path, 'wb') as f:
+        pickle.dump(grid_search, f)
+
+def main():
+    trial(model='dt', drop_attributes=None, sampling='over', bins=10)
+
+if __name__ == '__main__':
+    main()
